52aed0208c257fa7bdf3a3699db7fd306d91b1ec
erlantoktosunov <erlantoktosunovfa@gmail.com>
2022-07-13 15:41:10 +0600

    first commit

---
+++ bin/homework.dart
@@ -0,0 +1,25 @@
+import 'package:homework/homework.dart' as homework;
+
+void main(List<String> arguments) {
+  double b=4.20;
+  int a=3;
+  // double c; 
+  // c=a+b;  
+  //print(c)           
+  print(a+b);
+
+
+  String text='name';
+  text='flutter';
+  print("I want to learn $text");
+
+
+
+  double myNumber=2.225;
+  String myText;
+  myText = myNumber.toString();
+  print(myText+myNumber);
+
+  
+
+}
